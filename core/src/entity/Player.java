4d102bd1a6f651b8e62d967c10462bd48c76a7d1
jxxyx <jxxyx21@gmail.com>
2024-03-20 19:28:37 +0800

    edits

--- core/src/entity/Player.java
+++ core/src/entity/Player.java
@@ -147,6 +147,9 @@ public class Player extends Entity{
 	// CHECK OBJECT NAME
 	switch (objectName) {
 		case "Key":
+		case "Answer 3":
+		case "Answer 5":
+		case "Answer 8":
 		case "Boots":
 			// CHECK IF INVENTORY IS FULL
 			if (inventory.size() != maxInventorySize) {
